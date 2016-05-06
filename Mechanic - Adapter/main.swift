838ee420b06f9ca0a561b01af7d20471208539a4
Reza Shirazian <reza.shirazian@gmail.com>
2016-05-05 23:31:17 -0700

    Initial Commit

---
+++ Mechanic - Adapter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mechanic - Adapter
+//
+//  Created by Reza Shirazian on 2016-05-05.
+//  Copyright © 2016 Reza Shirazian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
